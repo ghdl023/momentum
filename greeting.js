de69930b2167dc30accd20943bffa21da1003329
gwee <gwee1216@naver.com>
2018-11-11 14:27:24 +0900

    Saving the User Name

---
+++ greeting.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function saveName(text) {
+  localStorage.setItem(USER_LS, text); //브라우저 닫았따열어도 유지가됨
+}
+
+function handleSubmit(event) {
+  event.preventDefault(); // 엔터쳐도 새로고침 안됨
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello, ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    // she is not
+    askForName();
+  } else {
+    // she is
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
